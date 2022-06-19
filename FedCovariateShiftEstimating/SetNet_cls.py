78f30f09b4236630cdfe4bff39eb6f0e240b965f
Zifeng Tang <tangzifeng37@yeah.net>
2022-06-19 15:33:16 +0100

    Update: modify setnet to make it compatible to the FedCSE, including use instancenorm to replace batchnorm, reset the dimension of layers

--- FedCovariateShiftEstimating/SetNet_cls.py
+++ FedCovariateShiftEstimating/SetNet_cls.py
@@ -1,31 +1,32 @@
 import torch.nn as nn
 import torch.utils.data
 import torch.nn.functional as F
-from SetEncoders import PointNetEncoder, feature_transform_reguliarzer
+from FedCovariateShiftEstimating.SetEncoders import PointNetEncoder, feature_transform_reguliarzer
 
-class get_model(nn.Module):
-    def __init__(self, k=40, normal_channel=True):
-        super(get_model, self).__init__()
+class SetNet(nn.Module):
+    def __init__(self, k=6, normal_channel=True):
+        super(SetNet, self).__init__()
         if normal_channel:
-            channel = 6
+            channel = 40
         else:
-            channel = 3
+            channel = 40
         self.feat = PointNetEncoder(global_feat=True, feature_transform=True, channel=channel)
         self.fc1 = nn.Linear(1024, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, k)
         self.dropout = nn.Dropout(p=0.4)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
+        self.bn1 = nn.InstanceNorm1d(512)
+        self.bn2 = nn.InstanceNorm1d(256)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x, trans, trans_feat = self.feat(x)
+        global_feat = x
         x = F.relu(self.bn1(self.fc1(x)))
         x = F.relu(self.bn2(self.dropout(self.fc2(x))))
         x = self.fc3(x)
         x = F.log_softmax(x, dim=1)
-        return x, trans_feat
+        return x, trans_feat, global_feat
 
 class get_loss(torch.nn.Module):
     def __init__(self, mat_diff_loss_scale=0.001):
