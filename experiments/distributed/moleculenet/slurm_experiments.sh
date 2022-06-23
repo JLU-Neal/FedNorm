e42305e90f7a35f0815cc771d8fb46da2457ccd4
Zifeng Tang <tangzifeng37@yeah.net>
2022-06-23 16:27:28 +0100

    Fix: export variable

--- experiments/distributed/moleculenet/slurm_experiments.sh
+++ experiments/distributed/moleculenet/slurm_experiments.sh
@@ -18,10 +18,11 @@ source /vol/bitbucket/{USER}/miniconda/etc/profile.d/conda.sh
 conda activate fedgraphnn
 . /vol/cuda/11.1.0-cudnn8.0.4.30/setup.sh
 
+export I_MPI_PROCESS_MANAGER=mpd
 mpirun -np $PROCESS_NUM -hostfile ./mpi_host_file python3 main_fedavg.py \
   --model $MODEL \
   --fl_algorithm $FL_ALG \
   --dataset $DATASET \
   --partition_method $DISTRIBUTION  \
 
-  uptime
+uptime
