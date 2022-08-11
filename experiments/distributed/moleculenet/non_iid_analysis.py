bbca18f2cf0fde84c375f0f287a5cf34e0e15280
Zifeng Tang <zt520@ic.ac.uk>
2022-08-11 20:40:35 +0100

    Update: add analysis_avg_shortest_path_length

--- experiments/distributed/moleculenet/non_iid_analysis.py
+++ experiments/distributed/moleculenet/non_iid_analysis.py
@@ -42,7 +42,7 @@ def degree_distribution(graphs: list):
     avg_degree = sum_degree / count_degree
     return overall_hist, avg_degree
 
-def analysis_degree_distribution(graphs: list, avg_degree: float):
+def analysis_degree_distribution(graphs: list, popmean: float):
     # kurtosis of degree distribution
     overall_hist, _ = degree_distribution(graphs)
     kur = kurtosis(overall_hist)
@@ -50,12 +50,38 @@ def analysis_degree_distribution(graphs: list, avg_degree: float):
     #  t-test for degree distribution
     sample_observation = [[idx] * int(overall_hist[idx]) for idx in range(overall_hist.size)]
     sample_observation = [item for sublist in sample_observation for item in sublist]
-    stattistic, p_value = ttest_1samp(sample_observation, avg_degree)
+    stattistic, p_value = ttest_1samp(sample_observation, popmean)
 
     return kur, p_value
 
 def avg_shortest_path_length(graphs: list):
-    pass
+    avg_length_each_graph = []
+    for graph in graphs:
+        sum_avg_path = 0
+        sum_weight = 0
+        for C in (graph.subgraph(c).copy() for c in nx.connected_components(graph)):
+            ele_num = len(C.nodes)
+            #  weight is calculated based on the number of paths 
+            #  between every pair of two node in a component
+            weight = (1+ele_num) * ele_num / 2
+            sum_weight += weight
+            sum_avg_path += nx.average_shortest_path_length(C) * weight
+
+        avg_shortest_path_length = sum_avg_path / sum_weight
+        avg_length_each_graph.append(avg_shortest_path_length)
+
+    avg_len = sum(avg_length_each_graph) / len(avg_length_each_graph)
+
+    return avg_length_each_graph, avg_len
+    
+        
+def analysis_avg_shortest_path_length(graphs: list, popmean: float):
+    avg_length_each_graph, avg_len = avg_shortest_path_length(graphs)
+    
+    # t-test for avg shortest path length
+    stattistic, p_value = ttest_1samp(avg_length_each_graph, popmean)
+
+    return avg_len, p_value
 
 
 def visualize_graph(graph: nx.Graph):
@@ -82,15 +108,16 @@ if __name__ == "__main__":
     graphs_chunked = [graphs[i:i+chunk_size] for i in range(0, len(graphs), chunk_size)]
 
 
-    avg_degree = degree_distribution(graphs)[1]
+    popmean_degree = degree_distribution(graphs)[1]
+    popmean_avg_shortest_path_length = avg_shortest_path_length(graphs)[1]
     for idx in range(chunk_num):
         graphs_sublist = graphs_chunked[idx]
 
-        kur, p_value_deg_dis = analysis_degree_distribution(graphs_sublist, avg_degree)
-        print("kurtosis: {}, p-value: {}".format(kur, p_value_deg_dis))
+        # kur, p_value_deg_dis = analysis_degree_distribution(graphs_sublist, popmean_degree)
+        # print("kurtosis: {}, p-value: {}".format(kur, p_value_deg_dis))
 
-        avg_path_len = avg_shortest_path_length(graphs_sublist)
-        print(avg_path_len)
+        avg_path_len, p_value_avg_len = analysis_avg_shortest_path_length(graphs_sublist, popmean_avg_shortest_path_length)
+        print("avg shortest path length: {}, p-value: {}".format(avg_path_len, p_value_avg_len))
     
     pass
 
