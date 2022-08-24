61d2f5a2442645cdbad12bbadabcebfe96801889
Zifeng Tang <zt520@ic.ac.uk>
2022-08-24 01:13:40 +0100

    Tune

--- experiments/distributed/moleculenet/batch_experiments.py
+++ experiments/distributed/moleculenet/batch_experiments.py
@@ -5,12 +5,17 @@ import logging
 # sh run_fedavg_distributed_pytorch.sh graphsage FedAvg sider hetero 3
 
 models = ['graphsage']
-fed_algs = ['FedAvg', 'FedAvg_DataSharing', 'FedAvg_FedNorm']
-datasets = ['sider', 'bbbp', 'bace', 'Tox21', 'clintox']
-data_distributions = ['hetero']
-client_nums = ['3']
 
-for model, fed_alg, dataset, datadistribution, client_num in list(itertools.product(models, fed_algs, datasets, data_distributions, client_nums)):
+# fed_algs = ['FedAvg', 'FedAvg_DataSharing', 'FedAvg_FedNorm']
+fed_algs = ['FedAvg_FedNorm']
+# datasets = ['sider', 'bbbp', 'bace', 'Tox21', 'clintox']
+datasets = ['Tox21', 'clintox']
+
+data_distributions = ['hetero']
+client_nums = ['3'] # should be set to 2 later
+p_alpha_factors = ['1']
+
+for model, fed_alg, dataset, datadistribution, client_num, p_alpha_factor in list(itertools.product(models, fed_algs, datasets, data_distributions, client_nums, p_alpha_factors)):
     logging.info("===================================================")
     logging.info("Running model: {}".format(model))
     logging.info("Running FedAlg: {}".format(fed_alg))
@@ -19,9 +24,9 @@ for model, fed_alg, dataset, datadistribution, client_num in list(itertools.prod
     logging.info("Running client num: {}".format(client_num))
     logging.info("===================================================") 
     if fed_alg == 'FedAvg':   
-        subprocess.run("sh run_fedavg_distributed_pytorch.sh".split() + [model, fed_alg, dataset, datadistribution, 'False', 'False', client_num])
+        subprocess.run("sh run_fedavg_distributed_pytorch.sh".split() + [model, fed_alg, dataset, datadistribution, 'False', 'False', client_num, p_alpha_factor])
     elif fed_alg == 'FedAvg_DataSharing':
-        subprocess.run("sh run_fedavg_distributed_pytorch.sh".split() + [model, fed_alg, dataset, datadistribution, 'True', 'False', client_num])
+        subprocess.run("sh run_fedavg_distributed_pytorch.sh".split() + [model, fed_alg, dataset, datadistribution, 'True', 'False', client_num, p_alpha_factor])
     elif fed_alg == 'FedAvg_FedNorm':
-        subprocess.run("sh run_fedavg_distributed_pytorch.sh".split() + [model, fed_alg, dataset, datadistribution, 'False', 'True', client_num])
+        subprocess.run("sh run_fedavg_distributed_pytorch.sh".split() + [model, fed_alg, dataset, datadistribution, 'False', 'True', client_num, p_alpha_factor])
 
